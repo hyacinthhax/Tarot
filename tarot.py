7059f79cefe5aef77d5f3ab4745bd6ab98e584f4
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2021-07-11 13:15:47 -0400

    Update tarot.py
    Updated Selection 3 for Use

--- tarot.py
+++ tarot.py
@@ -2,14 +2,18 @@ import random
 import time
 import os
 
+mycards = []
+file = []
+
 
 def run():
+    global randnum, randnum0
+    randnum0 = random.randint(1, 22)
+    randnum = random.randint(1, 22)
     question = input("What Must You Know?: ")
     game = input(
         "What Reading Do You Want?: \n 1: Single Card \n 2: Past, Present, Future \n 3: 4-Card Spread \n Selection: ")
-    file = []
     with open("tarotcards.txt") as f:
-        randnum = random.randint(1, 22)
         for line in f.readlines():
             file.append(line)
 
@@ -43,7 +47,81 @@ def run():
             run()
 
         elif int(game) == 3:
+            fourcard()
+
+
+def fourcard():
+    def rerun(selection):
+        print("Shuffling... ")
+        time.sleep(5)
+        if randnum0 != selection:
+            print(file[selection])
+
+        elif randnum0 == selection:
+            pass
+
+    rerun(randnum)
+
+    def choicenum1():
+        global randnum2
+        choice = input("Do You Like Your Card?: ").lower()
+        randnum2 = random.randint(1, 22)
+        if choice == 'yes':
+            print("Shuffling... ")
+            time.sleep(5)
+            mycards.append("First Card: " + file[randnum])
+            print(file[randnum2])
+            choicenum2()
+
+        elif choice == 'no':
+            rerun(randnum2)
+            choicenum1()
+
+    def choicenum2():
+        global randnum3
+        choice2 = input("Do you like this card?: ").lower()
+        randnum3 = random.randint(1, 22)
+        if choice2 == 'yes':
+            print("Shuffling... ")
+            time.sleep(5)
+            mycards.append("Second Card: " + file[randnum2])
+            print(file[randnum3])
+            choicenum3()
+
+        elif choice2 == 'no':
+            rerun(randnum3)
+            choicenum2()
+
+    def choicenum3():
+        global randnum4
+        choice3 = input("How About This Card?: ").lower()
+        randnum4 = random.randint(1, 22)
+        if choice3 == 'yes':
+            print("Shuffling... ")
+            time.sleep(5)
+            mycards.append("Third Card: " + file[randnum3])
+
+            print(file[randnum4])
+            choicenum4()
+
+        elif choice3 == 'no':
+            rerun(randnum3)
+
+    def choicenum4():
+        choice4 = input("What about this last card?")
+        if choice4 == 'yes':
+            mycards.append("Last Card: " + file[randnum4])
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(mycards[0] + '\n')
+            print(mycards[1] + '\n')
+            print(mycards[2] + '\n')
+            print(mycards[3] + '\n')
             run()
+
+        elif choice4 == 'no':
+            rerun(randnum4)
+
+    choicenum1()
 
 
 run()
